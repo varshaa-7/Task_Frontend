c5fd87f75800d3d8ea402442155a633b60b21d36
Varsha <awasthivarsha0710@gmail.com>
2024-09-13 21:14:20 +0530

    sec

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const baseURL = `http://localhost:5000/api`
-// export const baseURL=`https://newzyy-back.onrender.com/api`
+// export const baseURL = `http://localhost:5000/api`
+export const baseURL=`https://task-backend-2-53bj.onrender.com/api`
